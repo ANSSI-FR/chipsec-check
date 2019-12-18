f74f94e125fbbd713ac74de1293f00aead316170
Yves-Alexis Perez <yves-alexis.perez@ssi.gouv.fr>
2019-12-18 15:23:37 +0100

    tab-indent

--- tools/create-shell.sh
+++ tools/create-shell.sh
@@ -37,74 +37,74 @@ check_requirements() {
 }
 
 part_disk () {
-    disk="${1}"
+	disk="${1}"
 
-    parted "${1}" --script mklabel gpt
-    parted "${1}" --script mkpart ESP fat32 1MiB 100%
-    parted "${1}" --script set 1 boot on
+	parted "${1}" --script mklabel gpt
+	parted "${1}" --script mkpart ESP fat32 1MiB 100%
+	parted "${1}" --script set 1 boot on
 }
 
 umount_key () {
-    umount "${mount_point}"
+	umount "${mount_point}"
 }
 
 install_ca () {
-    mkdir "${mount_point}"/keytool
-    cp ca/*.auth ca/*.esl "${mount_point}"/keytool
-    mkdir "${mount_point}"/builtin
-    cp ca/*.cer "${mount_point}"/builtin
+	mkdir "${mount_point}"/keytool
+	cp ca/*.auth ca/*.esl "${mount_point}"/keytool
+	mkdir "${mount_point}"/builtin
+	cp ca/*.cer "${mount_point}"/builtin
 }
 
 install_shell () {
-    EFI="${mount_point}/EFI/Boot/BOOTX64.EFI"
-    mkdir -p "${mount_point}"/EFI/Boot/
-    cp bin/Shell.efi "${EFI}.tmp"
-    sbsign --key ca/DB.key --cert ca/DB.crt --output "${EFI}" "${EFI}.tmp"
-    rm "${EFI}.tmp"
+	EFI="${mount_point}/EFI/Boot/BOOTX64.EFI"
+	mkdir -p "${mount_point}"/EFI/Boot/
+	cp bin/Shell.efi "${EFI}.tmp"
+	sbsign --key ca/DB.key --cert ca/DB.crt --output "${EFI}" "${EFI}.tmp"
+	rm "${EFI}.tmp"
 }
 
 install_key_tool () {
-    KEFI="${mount_point}/KeyTool.EFI"
-    HEFI="${mount_point}/HelloWorld.EFI"
+	KEFI="${mount_point}/KeyTool.EFI"
+	HEFI="${mount_point}/HelloWorld.EFI"
 
-    cp /usr/lib/efitools/x86_64-linux-gnu/KeyTool.efi "${KEFI}.tmp"
-    sbsign --key ca/DB.key --cert ca/DB.crt --output "${KEFI}" "${KEFI}.tmp"
-    rm "${KEFI}.tmp"
+	cp /usr/lib/efitools/x86_64-linux-gnu/KeyTool.efi "${KEFI}.tmp"
+	sbsign --key ca/DB.key --cert ca/DB.crt --output "${KEFI}" "${KEFI}.tmp"
+	rm "${KEFI}.tmp"
 
-    cp /usr/lib/efitools/x86_64-linux-gnu/HashTool.efi "${HEFI}"
+	cp /usr/lib/efitools/x86_64-linux-gnu/HashTool.efi "${HEFI}"
 }
 
 main () {
-		check_requirements || exit 1
-    arg="${1}"
-		if [ -b "${arg}" ]; #block device, just use it directly
+	check_requirements || exit 1
+	arg="${1}"
+	if [ -b "${arg}" ]; #block device, just use it directly
+	then
+		target=${arg}
+		sep=""
+	else
+		if [ ! -f "${arg}" ]; # not a regular file, create it manually
 		then
-			target=${arg}
-			sep=""
-		else
-			if [ ! -f "${arg}" ]; # not a regular file, create it manually
-			then
-				truncate -s 4M ${arg}
-			fi
-			target=$(losetup --find --show "$arg")
-			sep="p"
+			truncate -s 4M ${arg}
 		fi
+		target=$(losetup --find --show "$arg")
+		sep="p"
+	fi
 
-    mount_point=$(mktemp -d -p "" efishellXXX)
-    part_disk "${target}"
-		sleep 1
-    mkfs.vfat "${target}${sep}1"
+	mount_point=$(mktemp -d -p "" efishellXXX)
+	part_disk "${target}"
+	sleep 1
+	mkfs.vfat "${target}${sep}1"
 
-    mount "${target}${sep}1" ${mount_point}
-    install_ca
-    install_shell
-    install_key_tool
-    umount_key
-    rmdir "${mount_point}"
-		if [ ! -b "$arg" ];
-		then
-			losetup -d $target
-		fi
+	mount "${target}${sep}1" ${mount_point}
+	install_ca
+	install_shell
+	install_key_tool
+	umount_key
+	rmdir "${mount_point}"
+	if [ ! -b "$arg" ];
+	then
+		losetup -d $target
+	fi
 }
 
 main "${1}"
