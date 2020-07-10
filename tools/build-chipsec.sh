91d7d20eb1a8507d686a9a3b6bbc89b676d13448
Yves-Alexis Perez <yves-alexis.perez@ssi.gouv.fr>
2020-07-10 15:41:25 +0200

    explicitely build chipsec kernel module
    starting with 1.5.2 (66f93f1b3d179f09e51d3d3a04707b4e03abad31) kernel module is
    not built when only calling build or install, so explicitely ask for it

--- tools/build-chipsec.sh
+++ tools/build-chipsec.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 current_kernel () {
-    #find /usr/lib/modules -maxdepth 2 -name build -type d
     find /lib/modules -maxdepth 2 -name build
 }
 
 cd /root/chipsec
-KERNEL_SRC_DIR="$(current_kernel)" python3 setup.py install
+KERNEL_SRC_DIR="$(current_kernel)" python3 setup.py build build_ext install
