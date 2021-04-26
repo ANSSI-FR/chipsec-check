8d0b1ada4f601f1c59f1c1ae37f3aa96a4e64d05
Yves-Alexis Perez <yves-alexis.perez@ssi.gouv.fr>
2021-04-26 10:56:17 +0200

    build-chipsec: update variable override for latest chipsec

--- tools/build-chipsec.sh
+++ tools/build-chipsec.sh
@@ -5,4 +5,4 @@ current_kernel () {
 }
 
 cd /root/chipsec
-KERNEL_SRC_DIR="$(current_kernel)" python3 setup.py build build_ext install
+KSRC="$(current_kernel)" python3 setup.py build build_ext install
